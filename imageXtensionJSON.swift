57afa484c2e17af1e1db51ccc918e03cc2158708
IPL <38543275+izloop@users.noreply.github.com>
2019-05-17 16:54:30 -0400

    AlamofireImage
    add the file for management of image upload using AlamofireImage

---
+++ imageXtensionJSON.swift
@@ -0,0 +1,41 @@
+//
+//  imageXtensionJSON.swift
+//  fixer
+//
+//  Created by Izloop on 5/17/19.
+//  Copyright © 2019 Peter Levi Hornig. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+import AlamofireImage
+
+//MARK: - Image Cache
+
+let imageCache = NSCache<NSString, UIImage>()
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(_ urlString: String) {
+        
+        self.image = nil
+        
+        //check for image first
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+            self.image = cachedImage
+            return
+        }
+        
+        //otherwise fire off a new download
+        Alamofire.request(urlString)
+            .responseImage { response in
+                
+                if let downloadedImage = response.result.value {
+                    //image is here
+                    imageCache.setObject(downloadedImage, forKey: urlString as NSString)
+                    self.image = downloadedImage
+                }
+        }
+    }
+}
